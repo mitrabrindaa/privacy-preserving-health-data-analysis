d4edaa0eb4cc147b916bf1fccb0a0418b3c17b5a
Mitrabrinda <mitrabrindasjd24@gmail.com>
2024-08-07 01:21:25 +0530

    Initial commit

---
+++ src/data_exploration.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv(r'C:\Users\dell pc\privacy-preserving-health-data-analysis\data\Disease_symptom_and_patient_profile_dataset.csv')
+
+
+# Display the first few rows
+print(df.head())
